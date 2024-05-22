52b362b1195d561bfbb53e67cb1115560a81d4c7
Shamsul Arefin <shamsularefin500@gmail.com>
2024-05-22 13:43:12 +0600

    Dist added again

---
+++ dist/modules/order/order.interface.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// "email": "level2@programming-hero.com",
+//     "productId": "5fd67e890b60c903cd8544a3",
+//     "price": 999,
+//     "quantity": 1
